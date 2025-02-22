7af3149ec18890f48371ffa9243714f0bfd4a9d3
Zulqarnain-G <zulqarnaingujjar537@gmail.com>
2025-02-22 17:44:01 +0500

    GIT bash added

---
+++ ssas.py
@@ -0,0 +1,18 @@
+
+
+try:
+    first_number = input("Enter first number: ")  
+    result = 10/first_number            
+except ZeroDivisionError:
+    print("cannot divide by zero")
+except ValueError:
+    print("Invalid Input")   
+except TypeError:
+    print("Type Error")                                                   
+except Exception as e:
+     print("Exception raised ", e)
+     print("Exception type ", type(e))
+else:
+    print("No exception occurred")
+    
+print("This is the end of the program")
